0799f2874ce919a829500d212e175aa660f3ff15
Niklas Rueber <nrueber@igus.net>
2021-12-14 17:55:26 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- hand_test.py
+++ hand_test.py
@@ -1,9 +1,11 @@
 import unittest
 from hand import *
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(hand(["K♠", "A♦"], ["J♣", "Q♥", "9♥", "2♥", "3♦"]), ('nothing', ['A', 'K', 'Q', 'J', '9']))  # add assertion here
+class Tests_Hand_worksAsExpected(unittest.TestCase):
+    def test_hand_nothing_returns_type_nothing_and_correct_sequence(self):
+        actual = hand(["K♠", "A♦"], ["J♣", "Q♥", "9♥", "2♥", "3♦"])
+        expected = ('nothing', ['A', 'K', 'Q', 'J', '9'])
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
