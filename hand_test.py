4bad840faf0ec16ed3330996302dda4a2b27d3c0
Falk Kühnel <falk@kuehnel.eu>
2021-12-14 17:35:54 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- hand_test.py
+++ hand_test.py
@@ -1,10 +1,9 @@
 import unittest
-
+from hand import *
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
+        self.assertEqual(hand(["K♠", "A♦"], ["J♣", "Q♥", "9♥", "2♥", "3♦"]), ('nothing', ['A', 'K', 'Q', 'J', '9']))  # add assertion here
 
 if __name__ == '__main__':
     unittest.main()
